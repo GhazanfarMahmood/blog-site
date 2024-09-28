9b936495ba59cab86b0a532ca6dc028129a1e72c
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2024-09-28 12:31:28 +0500

    Folder structure created

---
+++ src/components/blog-card.tsx
@@ -0,0 +1,3 @@
+export default function BlogCard() {
+  return <h1>This is the blog card</h1>;
+}
---
+++ src/pages/create-post/page.tsx
@@ -0,0 +1,3 @@
+export default function CreatePost() {
+  return <h1>This is the home page</h1>;
+}
